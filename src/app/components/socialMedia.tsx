70ce4edf1bc6cabfd95ae395035721101b91a661
arthue de souza silva <arthurdesouza.silv@gmail.com>
2023-06-16 00:37:03 -0300

    add curriculo

--- src/app/components/socialMedia.tsx
+++ src/app/components/socialMedia.tsx
@@ -10,7 +10,7 @@ export function SocialMedia() {
       <Link href={'https://github.com/ArthurSilv4'} className="mx-4">
         <BsGithub size={30} color="" />
       </Link>
-      <Link href={''}>
+      <Link href={'Currículo - Arthur De Souza.pdf'}>
         <BsFillFileTextFill size={30} color="" />
       </Link>
     </>
